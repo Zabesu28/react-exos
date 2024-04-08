0518dd2c1869b5d7e32ed82877968a8b7e8abccd
Zabesu <112702309+Zabesu28@users.noreply.github.com>
2024-04-08 14:56:45 +0200

    <feature>(sémantique) : ajout balise

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,12 @@ function Header() {
     return (
       <header>
         <img src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" 
-        alt="Singe mexicain qui fait de la guitare"/>
-        <li>Page d'accueil</li>
+        alt="Mexicain qui fait de la guitare"/>
+        <nav>
+            <ul>
+                <li><a href="/">Accueil</a></li>
+            </ul>
+        </nav>
       </header>
     );
   }
