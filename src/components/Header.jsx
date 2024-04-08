602c4531dbe753c483e9276f5965b22ad4968ad2
Zabesu <112702309+Zabesu28@users.noreply.github.com>
2024-04-08 14:38:46 +0200

    <feature>(Header) : activite 4 fait

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+      <header>
+        <img src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" 
+        alt="Singe mexicain qui fait de la guitare"/>
+        <li>Page d'accueil</li>
+      </header>
+    );
+  }
+
+  export default Header;
