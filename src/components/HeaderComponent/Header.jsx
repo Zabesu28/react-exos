e36155a861a37ab00f32162526576fb80cd6118a
Zabesu <112702309+Zabesu28@users.noreply.github.com>
2024-04-09 10:38:41 +0200

    <feature>(asset) : v1

---
+++ src/components/HeaderComponent/Header.jsx
@@ -0,0 +1,30 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import styles from "./header.css";
+import logo from "../../assets/images/logo.webp";
+
+function Header() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            className="logo"
+            src={logo}
+            alt="Mexicain qui fait de la guitare"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="/">Accueil</Nav.Link>
+            <Nav.Link href="#propos">A propos</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
