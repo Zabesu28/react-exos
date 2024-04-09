7f283911617e0cf66c2e85729bcc837ddebedcb3
Zabesu <112702309+Zabesu28@users.noreply.github.com>
2024-04-09 14:10:38 +0200

    <feature>(Lien plat) : v1

--- src/components/DishComponent/Dish.jsx
+++ src/components/DishComponent/Dish.jsx
@@ -2,18 +2,20 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Col from 'react-bootstrap/Col';
 import styles from "./dish.css";
+import { Link } from 'react-router-dom';
 
-const Dish = ({ img, name, price }) => {
+const Dish = ({ img, name, price, slug }) => {
   return (
     <Col>
+    <Link to={"plat/" + slug}>
       <Card>
         <Card.Img className="card-img" variant="top" src={img} />
         <Card.Body>
           <Card.Title>{name}</Card.Title>
-          <Card.Text>Prix : {price} euros</Card.Text>
-          <Button variant="primary">Voir +</Button>
+          <Card.Text>{price} €</Card.Text>
         </Card.Body>
       </Card>
+      </Link>
     </Col>
   );
 };
