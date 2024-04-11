8a056bf3362642d069fe610dde491120c55030f4
Zabesu28 <romain.zabeth@gmail.com>
2024-04-11 10:57:38 +0200

    v1

--- src/pages/DishDetailsComponent/DishDetails.jsx
+++ src/pages/DishDetailsComponent/DishDetails.jsx
@@ -1,16 +1,45 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import jsonDishes from "../../datas/dishes.json";
+import React, { useEffect, useState } from "react";
+import styles from "./dishDetails.css";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 const DishDetails = () => {
   const { slug } = useParams();
+  const [dish, setDish] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getDishBySlug();
+  }, []);
+
+  function getDishBySlug() {
+    setDish(jsonDishes.find((dish) => dish.slug == slug));
+  }
+
   return (
-    <Container className="pt-5 pb-5">
-      <Row>
-        <h1>Slug : {slug}</h1>
-      </Row>
-    </Container>
+    <>
+      {dish ? (
+        <Container className="pt-5 pb-5 container-details">
+          <Row>
+            <Col>
+              <img className="card-img" variant="top" src={dish.img} />
+            </Col>
+            <Col>
+              <h1>{dish.name}</h1>
+              <p>{dish.description}</p>
+              <p><span> {dish.price} €</span></p>
+              <Button variant="primary">Commander</Button> 
+            </Col>
+          </Row>
+        </Container>
+      ) : <Row>
+        <h1></h1>
+        </Row>}
+    </>
   );
 };
 export default DishDetails;
